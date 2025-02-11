cedd14b73f0f853084b5366aa4aec11ddd87d874
Mukul2956 <162742623+Mukul2956@users.noreply.github.com>
2025-02-11 23:35:18 +0530

    Add files via upload

---
+++ kthElementOfTwoSortedArrays.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int kthElementOfTwoSortedArrays(vector<int>&arr1, vector<int>&arr2, int n1, int n2){
+
+}
+int main(){
+    vector<int>arr1 = {};
+    vector<int>arr2 = {};
+    int n1 = arr1.size();
+    int n2 = arr2.size();
+}
