0e13451e6d745459359acf2853b78f8f500608ad
Mukul Singh <162742623+Mukul2956@users.noreply.github.com>
2025-04-09 21:24:38 +0530

    Add files via upload

---
+++ searchInsert.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int searchInsert(vector<int>&arr,int target,int n){
+    int ans = n;
+    int low = 0;
+    int high = n-1;
+
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(arr[mid] >= target){
+            ans = mid;
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int>arr = {2,3,4,5,7,8};
+    int n = arr.size();
+    int lowerBoundIndex = searchInsert(arr,6,n);
+    cout << lowerBoundIndex;
+    return 0;
+}
